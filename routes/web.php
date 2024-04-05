ef316532b11e0e0908c6639dcd492d9205dc5cd5
Felipe Ferreira <100241185+felipeov1@users.noreply.github.com>
2024-04-05 03:23:08 -0300

    fix: bug

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,12 @@
 use App\Http\Controllers\NoticiasController;
 use Illuminate\Support\Facades\Route;
 
-// use App\Http\Controllers\ProfileController;
 
 
 Route::get('/', [NoticiasController::class, 'index'])->name('noticias.noticia');
 Route::get('/noticia/{noticia}', [NoticiasController::class, 'show'])->name('noticia.show');
 
 
-// Route::get('/notcia/{noticias}', [NoticiasController::class, 'show'])->name('noticia.show');
 
 
 
